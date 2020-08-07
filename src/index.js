44e415df34ee4da48f7b1bd66716e0ee710cb52d
nurrahmahsw <nurrahmahsww@gmail.com>
2020-08-07 15:32:45 +0700

    template v1 2

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import "./script/component/nav.js";
 import "../node_modules/materialize-css/dist/js/materialize.js"
 import "../node_modules/materialize-css/dist/css/materialize.css"
 import $ from "jquery"; 
+
+// memberi animasi pada bg header
+$('header').mousemove(function(e){
+    var moveX = (e.pageX * -1 / 50);
+    var moveY = (e.pageY * -1 / 50);
+    $(this).css('background-position', moveX + 'px ' + moveY + 'px')
+    $(this).css('width', '100%')
+})
