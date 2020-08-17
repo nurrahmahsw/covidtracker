858200eb30727b375d273e7fa1206e145e2e26cf
nurrahmahsw <nurrahmahsww@gmail.com>
2020-08-17 18:03:24 +0700

    last commit

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        publicPath: '/src/',
+        publicPath: '/src/temp.html',
         filename: "bundle.js"
     },
     module: {
